fa7dd5e2aa214c072d8e22b6f0236b7c5f35cd5a
Sloth <sepcalamar@gmail.com>
2023-12-01 15:45:36 -0300

    Schemas uses references

--- Schemas/matchSchema.js
+++ Schemas/matchSchema.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const matchSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User'},
     charSelected: String,
     banSelected: String,
     playedAt: Number,
