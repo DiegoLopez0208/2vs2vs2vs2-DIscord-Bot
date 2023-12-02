e8b69bedfd539a3f1a88c6762e1320cfcaf2b2bb
Sloth <sepcalamar@gmail.com>
2023-12-02 16:26:17 -0300

    Create-group command fix

--- commands/interaction/create-group.js
+++ commands/interaction/create-group.js
@@ -1,21 +1,21 @@
 import pkg from 'discord.js';
 const { SlashCommandBuilder } = pkg;
-import { GroupSchema } from '../../Schemas/groupSchema.js'
+import { TeamSchema } from '../../Schemas/teamSchema.js'
 import { UserSchema } from '../../Schemas/userSchema.js';
 import "../../config/dotenv.js";
 
 export const data = new SlashCommandBuilder()
-  .setName('grupo')
-  .setDescription('Crear grupo!')
+  .setName('equipo')
+  .setDescription('Crear equipo!')
   .addUserOption(option => option.setName('integrante1').setDescription('Tu usuario'))
   .addUserOption(option => option.setName('integrante2').setDescription('El usuario de tu compañero'))
-  .addStringOption(option => option.setName('nombre_grupo').setDescription('El nombre de su grupo!'));
+  .addStringOption(option => option.setName('nombre_equipo').setDescription('El nombre de su equipo!'));
 
 
 export async function execute(interaction) {
   const integrante1 = interaction.options.getUser('integrante1');
   const integrante2 = interaction.options.getUser('integrante2');
-  const nombreGrupo = interaction.options.getString('nombre_grupo');
+  const name = interaction.options.getString('nombre_equipo');
 
   if (integrante1.username != interaction.user.username) {
     return interaction.reply({ content: '¡Tu tienes que ser el primer usuario!', ephemeral: true});
@@ -26,6 +26,10 @@ export async function execute(interaction) {
   if (!integrante2) {
     return interaction.reply({ content: 'Por favor, ingresa un usuario valido!', ephemeral: true});
   }
+  if (!name) {
+    return interaction.reply({ content: 'Ingrese un nombre del equipo!', ephemeral: true});
+  }
+
 
   const user1 = await UserSchema.findOne({discordTag: integrante1.username});
   const user2 = await UserSchema.findOne({discordTag: integrante2.username});
@@ -36,21 +40,29 @@ export async function execute(interaction) {
   if(!user2) {
     return interaction.reply({content: `❌ El usuario ${integrante2} no esta inscripto al evento! que utilice el comando /inscription para estarlo!`, ephemeral: true});
   }
-  /*
-    faltaria que verifique que el usuario no este en un grupo ya pero no se como hacerlo xD
-  
-    La verdad tendriamos que hacer una especia de archivo que sea como un handler de errores
-    al que le pasemos unos codigos en especifico dependiendo la condicion xd
-  */
 
-  const newGroup = new GroupSchema({
-    name: nombreGrupo,
+  const groups = await TeamSchema.find();
+
+  const user1IsInGroup = groups.some(group =>
+    user1._id.equals(group.memberid1) || user1._id.equals(group.memberid2)
+  );
+  
+  const user2IsInGroup = groups.some(group =>
+    user2._id.equals(group.memberid1) || user2._id.equals(group.memberid2)
+  );
+  
+  if (user1IsInGroup || user2IsInGroup) {
+    return interaction.reply({ content: `❌ Uno de los usuarios ya está inscrito en un equipo.`, ephemeral: true });
+  }
+
+  const newTeam = new TeamSchema({
+    name,
     memberid1: user1,
     memberid2: user2,
     icon: '',
     points: 0
   })
 
-  console.log(newGroup);
-  interaction.reply({ content: '¡uwu!', ephemeral: true});
+  await newTeam.save();
+  interaction.reply({ content: `El equipo "${name}" fue inscripto correctamente!`, ephemeral: true});
 }
